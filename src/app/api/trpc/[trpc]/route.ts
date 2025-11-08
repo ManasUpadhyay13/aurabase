52845f116b9c0b0f4e2f8d9710260b98c0077edc
Manas Upadhyay <manasupadhyay1318@gmail.com>
2025-11-08 22:20:31 +0530

    added trpc

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+// route is a reversed file name for server routing
+// just like the page.tsx is reversed for the frontend routing
+
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { createTRPCContext } from "@/trpc/init";
+import { appRouter } from "@/trpc/routers/_app";
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: createTRPCContext,
+  });
+export { handler as GET, handler as POST };
---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
+export const appRouter = createTRPCRouter({
+  hello: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query(() => {
+      return {
+        greeting: `hello world`,
+      };
+    }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
