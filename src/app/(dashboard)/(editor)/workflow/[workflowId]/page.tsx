ffba9daebdd6e2cae05b7b38409e7c8aff0ca1e0
Manas Upadhyay <manasupadhyay1318@gmail.com>
2025-11-13 23:31:03 +0530

    sidebar setup

---
+++ src/app/(dashboard)/(editor)/workflow/[workflowId]/page.tsx
@@ -0,0 +1,13 @@
+interface PageProps {
+  params: Promise<{
+    workflowId: string;
+  }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { workflowId } = await params;
+
+  return <div>Workflow id: {workflowId}</div>;
+};
+
+export default Page;
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import {
+  CreditCardIcon,
+  FolderOpenIcon,
+  HistoryIcon,
+  KeyIcon,
+  LogOutIcon,
+  StarIcon,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { group } from "console";
+
+const menuItems = [
+  {
+    title: "Workflows",
+    items: [
+      {
+        title: "Workflows",
+        icon: FolderOpenIcon,
+        url: "/workflows",
+      },
+    ],
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        {menuItems.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupContent>
+              {group.items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    isActive={false}
+                    asChild
+                    className="gap-x-4 h-10 px-4"
+                  >
+                    <Link href={item.url} prefetch>
+                      <item.icon className="size-4" />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
