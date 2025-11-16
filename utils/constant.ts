d8c485ff1323d5bd9eda838b707c99112caa1937
Manas Upadhyay <manasupadhyay1318@gmail.com>
2025-11-16 23:27:58 +0530

    small commit

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,4 +1,5 @@
 export const APP_NAME = "Aurabase";
+
 export const APP_LOGO_URL = "/logo.svg";
 export const APP_DESCRIPTION =
   "Aurabase is a platform for creating generating agentic workflows.";
